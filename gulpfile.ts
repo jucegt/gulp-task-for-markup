525d172f6961f5752e4f0c603623322f1e10d004
jucegt <julio2201@gmail.com>
2021-08-27 23:35:06 -0600

    Changed gulp default task to call gulp path tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,110 +1,15 @@
 // Packages
-import { watch, series, parallel, src, dest } from 'gulp';
-import browserSync from 'browser-sync'
+import { series, parallel } from 'gulp';
 import color from 'ansi-colors';
 
-// Gulp Plugins
-import sourcemaps from 'gulp-sourcemaps';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import ts from 'gulp-typescript';
-// import uglify from 'gulp-uglify';
-// import rename from 'gulp-rename';
+// Tasks
+import { server, watcher } from './gulp/tasks';
+import { html, htmlmin } from './gulp/html';
+import { styles, stylesmin } from './gulp/styles';
+import { scripts, scriptsmin } from './gulp/scripts';
 
-// PostCSS Plugins
-import autoprefixer from 'autoprefixer';
-// import cssnano from 'cssnano';
-
-// Paths
-const paths = {
-  html: {
-    src: './src/*.html',
-    dest: './build/',
-  },
-  scss: {
-    src: './src/assets/scss/**/*.scss',
-    dest: './build/css/',
-  },
-  ts: {
-    src: './src/assets/ts/**/*.ts',
-    dest: './build/js/',
-  },
-};
-
+// Start gulp
 console.log(color.bold.yellow('<----------- Runing Tasks ----------->'));
-
-// Generate styles from scss
-const styles = () => {
-  console.log(color.bold.magenta('<--------------- SCSS --------------->'));
-
-  // Task
-  return src(paths.scss.src)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest(paths.scss.dest))
-    .pipe(browserSync.stream());
-};
-
-// Generate minified styles from scss
-const stylesmin = async () => {
-  console.log(color.bold.magenta('<--------------- SCSS --------------->'));
-  return;
-};
-
-// Generate scripts from ts
-const scripts = () => {
-  console.log(color.bold.blue('<---------------- TS ---------------->'));
-
-  // Config
-  const tsconfig = ts.createProject('tsconfig.json');
-  const reporter = ts.reporter.fullReporter();
-
-  const result = src(paths.ts.src)
-    .pipe(sourcemaps.init())
-    .pipe(tsconfig(reporter));
-
-  return result.js.pipe(sourcemaps.write('.')).pipe(dest(paths.ts.dest));
-}
-
-// Generate minified scripts from ts
-const scriptsmin = async () => {
-  console.log(color.bold.blue('<---------------- TS ---------------->'));
-  return;
-}
-
-// Move html to dist
-const html = () => {
-  console.log(color.bold.gray('<--------------- HTML --------------->'));
-  return src(paths.html.src).pipe(dest(paths.html.dest));
-}
-
-// Generate minified html
-const htmlmin = async () => {
-  console.log(color.bold.gray('<--------------- HTML --------------->'));
-  return;
-};
-
-// Watch files changes
-const watcher = () => {
-  // Watch html files changes
-  watch(paths.html.src).on('change', browserSync.reload);
-  // Watch scss files changes
-  watch(paths.scss.src, series(styles));
-  // Watch ts files changes
-  watch(paths.ts.src, series(scripts));
-}
-
-// BrowserSync
-const server = () => {
-  console.log(color.bold.cyan('<-------------- Server -------------->'));
-  browserSync.init({
-    server: {
-      baseDir: './build/',
-    },
-  });
-}
 
 // Develop
 const dev = series(html, styles, scripts, parallel(server, watcher));
